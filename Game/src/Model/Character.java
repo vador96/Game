a43eb9066e9ea86d53ddb00b26a0ab791ae329af
vador96 <andengui@ulb.ac.be>
2016-04-23 10:20:11 +0200

    Added files via upload

---
+++ Game/src/Model/Character.java
@@ -0,0 +1,71 @@
+package Model;
+
+import java.awt.*;
+
+public abstract class Character implements Collidable{
+
+    protected int posX;
+    protected int posY;
+    protected int speedX = 0; // vecteurs
+    protected int speedY = 0; // directeurs
+    protected int speed = 5;
+    private int health;
+    private Rectangle hitBox;
+
+    protected boolean moveLeft = true;
+    protected boolean moveRight = true;
+    protected boolean moveUp = true;
+    protected boolean moveDown = true;
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX*50; //  *50 : en reference a la taille de la map
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY*50;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        if (health >= 100) {
+            this.health = 100;
+        } else if (health <= 0) {
+            this.health = 0;
+        } else {
+            this.health = health;
+        }
+    }
+
+    public void move(int dx, int dy) {
+        if (dx < 0 && moveLeft) {
+            this.posX = posX + speed * dx;
+        } else if (dx > 0 && moveRight) {
+            this.posX = posX + speed * dx;
+        } else if (dy < 0 && moveUp) {
+            this.posY = posY + speed * dy;
+        } else if (dy > 0 && moveDown) {
+            this.posY = posY + speed * dy;
+        }
+        this.speedX = dx;
+        this.speedY = dy;
+    }
+}
---
+++ Game/src/Model/Player.java
@@ -0,0 +1,65 @@
+package Model;
+
+import java.awt.*;
+
+public class Player extends Character {
+
+    private Rectangle hitBox;
+
+    public Player(int x, int y, int hp) {
+        this.setPosX(x);
+        this.setPosY(y);
+        this.hitBox = new Rectangle(this.posX, this.posY, 50, 50);
+        this.setHealth(hp);
+
+    }
+
+    @Override
+    public Rectangle getHitbox() {
+        return hitBox;
+    }
+
+    @Override
+    public void setHitBox(int x, int y) {
+        this.hitBox.setRect(x, y, 50, 50);
+    }
+
+    @Override
+    public boolean collides(Collidable collidable) {
+        boolean collision;
+        Rectangle box = collidable.getHitbox();
+        if (this.hitBox.intersects(box)) {
+            collision = true;
+        } else {
+            collision = false;
+        }
+        return collision;
+    }
+
+    @Override
+    public void applyCollision(Collidable collidable) {
+        if (speedX < 0) {
+            moveLeft = false;
+            move(1,0);
+        } else if (speedX > 0) {
+            moveRight = false;
+            move(-1,0);
+        } else if (speedY < 0) {
+            moveUp = false;
+            move(0,1);
+        } else if (speedY > 0) {
+            moveDown = false;
+            move(0,-1);
+        }
+
+    }
+
+    public void update() {
+        move(speedX,speedY);
+        setHitBox(this.posX, this.posY);
+        moveDown = true;
+        moveLeft = true;
+        moveRight = true;
+        moveUp = true;
+    }
+}
