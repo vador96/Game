bab19fd0de37a0c380639522475b354b2c7d5fdd
BakaQuy <p.trongquy@gmail.com>
2016-04-28 23:16:26 +0200

    Added files via upload

--- src/Model/Gate.java
+++ src/Model/Gate.java
@@ -1,31 +1,30 @@
 package Model;
 
-import java.awt.*;
-
 public class Gate extends Decor {
 
-    private boolean open;
-    private String nameLevel;
+	private boolean open;
+	private String nameLevel;
 
-    public Gate(int x, int y, String nameLevel) {
-        super(x,y);
-        this.nameLevel = nameLevel;
-        this.open = true;
+	public Gate(int x, int y, String nameLevel) {
+		super(x, y);
+		setHitBox(x*40+37, y*40, 3, 40);
+		this.nameLevel = nameLevel;
+		this.open = true;
+	}
 
-    }
-    public boolean isOpen() {
-        return this.open;
-    }
+	public boolean isOpen() {
+		return this.open;
+	}
 
-    public void setOpen(boolean open) {
-        this.open = open;
-    }
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
 
-    public String getNameLevel() {
-        return nameLevel;
-    }
+	public String getNameLevel() {
+		return nameLevel;
+	}
 
-    public void setNameLevel(String nameLevel) {
-        this.nameLevel = nameLevel;
-    }
+	public void setNameLevel(String nameLevel) {
+		this.nameLevel = nameLevel;
+	}
 }
