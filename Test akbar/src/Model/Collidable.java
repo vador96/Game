d2a6fb882149f0c37d85634c40ee8e776a6451f0
BakaQuy <p.trongquy@gmail.com>
2016-04-30 17:16:43 +0200

    Added files via upload

---
+++ Test akbar/src/Model/Collidable.java
@@ -0,0 +1,24 @@
+package Model;
+
+import java.awt.*;
+
+public interface Collidable {
+	
+	public final int sizeSquare = 40;
+	
+	Rectangle getHitbox();
+
+	void setHitbox(int x, int y);
+
+	boolean collides(Collidable collidable);
+
+	void applyCollisionOn(Collidable collidable);
+
+	int collidesWith(Rectangle box);
+
+	void getDamageFromMonster(int damage);
+	
+	void getDamageFromPlayer(int damage);
+
+	void goBack(Rectangle hitBox);
+}
