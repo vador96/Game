6997fb5e2ce588bff6562ff2f07a3f92f7b1fdb2
BakaQuy <p.trongquy@gmail.com>
2016-05-06 16:10:41 +0200

    Added files via upload

---
+++ On est bon/src/Model/Player.java
@@ -0,0 +1,195 @@
+package Model;
+
+import java.util.ArrayList;
+
+import View.Window;
+
+public class Player extends Character {
+
+	private boolean readyToAttack = true;
+	private boolean invincible = false;
+
+	private boolean changingMap = false;
+	private int tick1;
+	private int tick2;
+	private int limitTick;
+	private final int TICK_CHANGE_MAP = 3;
+
+	private int attackStat = 40;
+	private int attack = 40;
+	private int manaAttack = 100;
+
+	private ArrayList<Potion> healPotions = new ArrayList<>();
+	private ArrayList<Potion> manaPotions = new ArrayList<>();
+
+	public Player(int x, int y, int speed, int hp, int mana, Game game) {
+		super(x, y, speed, hp, mana, game);
+		this.tick1 = 0;
+		this.tick2 = 0;
+	}
+
+	public void getDamage(int damage) {
+		int hp = this.getHealth();
+		if (!this.isInvincible()) {
+			this.setHealth(hp - damage);
+		}
+	}
+
+	public void attack() {
+		if (this.getMana() >= manaAttack) {
+			game.addProjectile(this.getPosX(), this.getPosY(), dir, speed * 3, this.attack);
+			useMana(manaAttack);
+		}
+	}
+
+	public void meleeAttack() {
+		game.addProjectile(this.getPosX() + 10, this.getPosY() + 10, dir, 0, 10);
+	}
+
+	public void update() {
+		super.update();
+		move(speedX, speedY);
+	}
+
+	public boolean isReadyToAttack() {
+		return readyToAttack;
+	}
+
+	public void setReadyToAttack(boolean readyToAttack) {
+		this.readyToAttack = readyToAttack;
+	}
+
+	@Override
+	public void notifyObserver(Observer observer) {
+		observer.update();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				update();
+				if (this.isInvincible() == true) {
+					tick1 += 1;
+				}
+				if (tick1 >= limitTick && this.isInvincible() == true) {
+					this.becomeNormal();
+				}
+				if (this.isChangingMap() == true) {
+					tick2 += 1;
+				}
+				if (tick2 >= TICK_CHANGE_MAP && this.isChangingMap() == true) {
+					this.setChangingMap(false);
+				}
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void acceptCollision(Collision collision) {
+		collision.applyCollisionOn(this);
+	}
+
+	@Override
+	public void applyCollisionOn(Block block) {
+		this.goBack(block);
+	}
+
+	@Override
+	public void applyCollisionOn(Potion potion) {
+		this.getBonus(potion);
+		potion.setConsummed(true);
+	}
+
+	public void getBonus(Potion potion) {
+		if (potion.getBonusHealth() != 0) {
+			healPotions.add(potion);
+		} else if (potion.getBonusMana() != 0) {
+			manaPotions.add(potion);
+		} else if (potion.isBonusInvincible() == true) {
+			becomeInvincible(500);
+		}
+	}
+
+	public void useHealPotion() {
+		if (healPotions.size() != 0) {
+			this.setHealth(this.getHealth() + healPotions.get(0).getBonusHealth());
+			healPotions.remove(0);
+		}
+	}
+
+	public void useManaPotion() {
+		if (manaPotions.size() != 0) {
+			this.setMana(this.getMana() + manaPotions.get(0).getBonusMana());
+			manaPotions.remove(0);
+		}
+	}
+
+	void becomeInvincible(int tickNumber) {
+		this.setInvincible(true);
+		this.setAttack(2 * attackStat);
+		this.tick1 = 0;
+		this.limitTick = tickNumber;
+	}
+
+	void becomeNormal() {
+		setInvincible(false);
+		this.setAttack(attackStat);
+	}
+
+	void changeMap() {
+		this.setChangingMap(true);
+		this.tick2 = 0;
+	}
+
+	@Override
+	public void applyCollisionOn(Gate gate) {
+		this.changeMap();
+		gate.setOpen(true);
+		if (gate.type == 'W') {
+			this.setPosX((int) (Window.WINDOW_WIDTH - 2 * this.getHitbox().getWidth()));
+		} else if (gate.type == 'E') {
+			this.setPosX((int) (this.getHitbox().getWidth() / 4.0));
+		} else if (gate.type == 'N') {
+			this.setPosY((int) (Window.WINDOW_HEIGHT - 2 * this.getHitbox().getWidth()));
+		} else if (gate.type == 'S') {
+			this.setPosY((int) (this.getHitbox().getWidth() / 4.0));
+		}
+		game.changeLevelTo(gate.type);
+	}
+
+	public boolean isInvincible() {
+		return invincible;
+	}
+
+	public void setInvincible(boolean invicible) {
+		this.invincible = invicible;
+	}
+
+	public boolean isChangingMap() {
+		return changingMap;
+	}
+
+	public void setChangingMap(boolean changingMap) {
+		this.changingMap = changingMap;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public ArrayList<Potion> getHealPotions() {
+		return healPotions;
+	}
+
+	public ArrayList<Potion> getManaPotions() {
+		return manaPotions;
+	}
+}
